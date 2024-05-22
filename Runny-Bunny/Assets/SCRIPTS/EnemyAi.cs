331e1122376e0b4fb44b8f838cc55cb0e1e8692b
padifalasanerd <163738111+padifalas@users.noreply.github.com>
2024-05-22 19:59:01 +0200

    Knockback on Gnome added

--- Runny-Bunny/Assets/SCRIPTS/EnemyAi.cs
+++ Runny-Bunny/Assets/SCRIPTS/EnemyAi.cs
@@ -5,30 +5,39 @@ public class EnemyAI : MonoBehaviour
     public GameObject player;
     public float speed;
     public float distanceBetween;
+    public float knockbackForce = 10f; // Knockback force to apply to the player
 
     private float distance;
-    //Start is called before the first frame update
 
+    // Start is called before the first frame update
     private void Start()
     {
-        
+        // Initialize if needed
     }
 
-    //Update is called once per frame 
+    // Update is called once per frame
     private void Update()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
-        Vector2 direction = player.transform.position - transform.forward;
-        direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //convert to a radiant to degree
-
+        Vector2 direction = (Vector2)(player.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // Convert to degrees
 
         if (distance < distanceBetween)
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
-        
     }
-    
-    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (playerRb != null)
+            {
+                Vector2 knockbackDirection = (collision.transform.position - transform.position).normalized;
+                playerRb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+            }
+        }
+    }
 }
