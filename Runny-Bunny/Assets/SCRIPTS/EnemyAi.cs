e4ad7b846f2b507609419684463bb05b2357e06d
ErinSephton <2509281@students.wits.ac.za>
2024-06-03 20:59:30 +0200

    gnome now chases player 2

--- Runny-Bunny/Assets/SCRIPTS/EnemyAi.cs
+++ Runny-Bunny/Assets/SCRIPTS/EnemyAi.cs
@@ -9,6 +9,7 @@ public class EnemyAI : MonoBehaviour
     public float knockbackForce = 10f; // Knockback force to apply to the player
 
     private float distance;
+    private float distance2;
 
     // Start is called before the first frame update
     private void Start()
@@ -19,14 +20,23 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
+        distance = Vector2.Distance(transform.position, player.transform.position);        
         Vector2 direction = (Vector2)(player.transform.position - transform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // Convert to degrees
+
+        distance2 = Vector2.Distance(transform.position, player2.transform.position);
+        Vector2 direction2 = (Vector2)(player2.transform.position - transform.position).normalized;
+        float angle2 = Mathf.Atan2(direction2.y, direction2.x) * Mathf.Rad2Deg; // Convert to degrees
 
         if (distance < distanceBetween)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }       
+
+        else if (distance2 < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player2.transform.position, speed * Time.deltaTime);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -41,7 +51,7 @@ public class EnemyAI : MonoBehaviour
             }
         }
 
-        /*if (collision.gameObject.CompareTag("Player2"))
+        if (collision.gameObject.CompareTag("Player2"))
         {
             Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
             if (playerRb != null)
@@ -49,6 +59,6 @@ public class EnemyAI : MonoBehaviour
                 Vector2 knockbackDirection = (collision.transform.position - transform.position).normalized;
                 playerRb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
             }
-        }*/
+        }
     }
 }
