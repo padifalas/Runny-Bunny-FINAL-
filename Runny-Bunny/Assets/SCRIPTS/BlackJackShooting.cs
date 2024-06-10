efe405393ef3ee8b5c76ccd3de9928b85dfa5994
ErinSephton <2509281@students.wits.ac.za>
2024-06-10 17:51:12 +0200

    Blackjacks work for player 2

--- Runny-Bunny/Assets/SCRIPTS/BlackJackShooting.cs
+++ Runny-Bunny/Assets/SCRIPTS/BlackJackShooting.cs
@@ -7,11 +7,14 @@ public class BlackJackShooting : MonoBehaviour
     public GameObject bullet;
     public Transform bulletPos;
     private GameObject player;
+    private GameObject player2;
     private float timer;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+
+        player2 = GameObject.FindGameObjectWithTag("Player2");
     }
 
     // Update is called once per frame
@@ -22,6 +25,20 @@ public class BlackJackShooting : MonoBehaviour
         Debug.Log(distance);
 
         if (distance < 10)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > 1.5)
+            {
+                timer = 0;
+                shoot();
+            }
+        }
+
+        float distance2 = Vector2.Distance(transform.position, player2.transform.position);
+        Debug.Log(distance2);
+
+        if (distance2 < 10)
         {
             timer += Time.deltaTime;
 
