289390b90c1f413f64437542574ced0ae88453cb
ErinSephton <2509281@students.wits.ac.za>
2024-06-10 20:37:04 +0200

    fixed the win panels
    Now they only show up for who ever won, even if the other player touches the carrot

--- Runny-Bunny/Assets/SCRIPTS/Carrot.cs
+++ Runny-Bunny/Assets/SCRIPTS/Carrot.cs
@@ -8,6 +8,9 @@ public class Carrot : MonoBehaviour
 
     public GameObject P2Win;
 
+    bool P1Won;
+
+    bool P2Won;
 
     // Start is called before the first frame update
     void Start()
@@ -15,18 +18,75 @@ public class Carrot : MonoBehaviour
         P1Win.SetActive(false);
 
         P2Win.SetActive(false);
+
+        P1Won = false;
+
+        P2Won = false;
+    }
+
+    public void Player1()
+    {
+        P1Won = true;
+
+        P2Won = false;
+        
+        P1Win.SetActive (true);
+
+        P2Win.SetActive (false);
+
+        Debug.Log("player1 wins");
+       
+    }
+
+    public void Player2()
+    {
+        P1Won = false;
+
+        P2Won = true;
+
+        P1Win.SetActive(false);
+
+        P2Win.SetActive(true);
+
+        Debug.Log("player2 wins");
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            P1Win.SetActive(true);
+            if (P1Won == false)
+            {
+                if (P2Won == false)
+                {
+                    Player1();
+
+                    P1Won = true;
+                }
+
+                else
+                {
+                    Debug.Log("Player2 already won");
+                }
+            }            
         }
 
         if (collision.gameObject.tag == "Player2")
         {
-            P2Win.SetActive(true);
+            if (P1Won == false)
+            {
+                if (P2Won == false)
+                {
+                    Player2();
+
+                    P2Won = true;
+                }
+
+                else
+                {
+                    Debug.Log("Player1 already won");
+                }
+            }                       
         }
     }
 
