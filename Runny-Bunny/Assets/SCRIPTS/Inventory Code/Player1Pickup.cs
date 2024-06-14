395e8dc65a4419eddb021293f059e8cf47229d2b
ErinSephton <2509281@students.wits.ac.za>
2024-06-14 23:18:29 +0200

    Revert "Reapply "Merge branch 'main' of https://github.com/padifalas/Runny-Bunny-FINAL-""
    This reverts commit 02e480940b0971d04c0b6689e4b9847be00a5f73.

--- Runny-Bunny/Assets/SCRIPTS/Inventory Code/Player1Pickup.cs
+++ Runny-Bunny/Assets/SCRIPTS/Inventory Code/Player1Pickup.cs
@@ -8,18 +8,23 @@ public class Player1Pickup : MonoBehaviour
     private Player1Inventory inventory;
 
     public GameObject StamCard;
-
     public GameObject HealthCard;
-
     public GameObject MentalCard;
 
     public GameObject CardPanel;
-
     public Button StamButton;
-
     public Button HealthButton;
-
     public Button MentalButton;
+
+    public GameObject CardPanel2;
+    public Button StamButton2;
+    public Button HealthButton2;
+    public Button MentalButton2;
+    
+    public GameObject CardPanel3;
+    public Button StamButton3;
+    public Button HealthButton3;
+    public Button MentalButton3;
 
     public int i;
     // Start is called before the first frame update
@@ -46,7 +51,6 @@ public class Player1Pickup : MonoBehaviour
             }
         }
     }
-
     public void OnHealthClick()
     {
         CardPanel.SetActive(false);
@@ -65,12 +69,123 @@ public class Player1Pickup : MonoBehaviour
             }
         }
     }
-
     public void OnMentalClick()
     {
         CardPanel.SetActive(false);
 
         MentalButton.interactable = false;
+
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.isFull[i] == false)
+            {
+                inventory.isFull[i] = true;
+
+                Instantiate(MentalCard, inventory.slots[i].transform, false);
+
+                break;
+            }
+        }
+    }
+
+
+    public void OnStaminaClick2()
+    {
+        CardPanel2.SetActive(false);
+
+        StamButton2.interactable = false;
+
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.isFull[i] == false)
+            {
+                inventory.isFull[i] = true;
+
+                Instantiate(StamCard, inventory.slots[i].transform, false);
+
+                break;
+            }
+        }
+    }
+    public void OnHealthClick2()
+    {
+        CardPanel2.SetActive(false);
+
+        HealthButton2.interactable = false;
+
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.isFull[i] == false)
+            {
+                inventory.isFull[i] = true;
+
+                Instantiate(HealthCard, inventory.slots[i].transform, false);
+
+                break;
+            }
+        }
+    }
+    public void OnMentalClick2()
+    {
+        CardPanel2.SetActive(false);
+
+        MentalButton2.interactable = false;
+
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.isFull[i] == false)
+            {
+                inventory.isFull[i] = true;
+
+                Instantiate(MentalCard, inventory.slots[i].transform, false);
+
+                break;
+            }
+        }
+    }
+
+
+    public void OnStaminaClick3()
+    {
+        CardPanel3.SetActive(false);
+
+        StamButton3.interactable = false;
+
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.isFull[i] == false)
+            {
+                inventory.isFull[i] = true;
+
+                Instantiate(StamCard, inventory.slots[i].transform, false);
+
+                break;
+            }
+        }
+    }
+    public void OnHealthClick3()
+    {
+        CardPanel3.SetActive(false);
+
+        HealthButton3.interactable = false;
+
+        for (int i = 0; i < inventory.slots.Length; i++)
+        {
+            if (inventory.isFull[i] == false)
+            {
+                inventory.isFull[i] = true;
+
+                Instantiate(HealthCard, inventory.slots[i].transform, false);
+
+                break;
+            }
+        }
+    }
+    public void OnMentalClick3()
+    {
+        CardPanel3.SetActive(false);
+
+        MentalButton3.interactable = false;
 
         for (int i = 0; i < inventory.slots.Length; i++)
         {
